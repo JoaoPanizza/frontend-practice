14a932171b736695d9afc178ae512ee9f436f061
JoaoPanizza <joaohenrique.panizzasilva07@gmail.com>
2026-02-13 11:11:43 -0300

    Bug fixes and mobile resolution
    Here I fixed some bugs in the submenu when the sidebar is closed and created the mobile layout.

--- 01-responsive-sidebar/script.js
+++ 01-responsive-sidebar/script.js
@@ -4,9 +4,30 @@ const sidebar = document.getElementById('sidebar')
 function toggleSidebar() {
     sidebar.classList.toggle('close')
     toggleButton.classList.toggle('rotate')
+
+    closeAllsubMenus()
 }
 
 function toggleSubMenu(button) {
+
+    if (window.screen.width <= 750) {
+        if (!button.nextElementSibling.classList.contains('show')) {
+            closeAllsubMenus()
+        }
+    }
+
     button.nextElementSibling.classList.toggle('show')
     button.classList.toggle('rotate')
+
+    if (sidebar.classList.contains('close')) {
+        sidebar.classList.toggle('close')
+        toggleButton.classList.toggle('rotate')
+    }
+}
+
+function closeAllsubMenus() {
+    Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+        ul.classList.remove('show')
+        ul.previousElementSibling.classList.remove('rotate')
+    })
 }
